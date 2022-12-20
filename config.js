cbb6a72ae6d1790fd22971de19ea3b69ca0f2ee5
iliaschymas <iliasx1911@gmail.com>
2022-12-20 13:18:51 +0200

    Whoops

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const configs = [
   {
-    consumer_key: 'qLwqBmlMnMaOd13zA1uD9jgEZ',
-    consumer_secret: 'ns7qgBk9BEkDj3f4l5QwASqy1vinkESlFuPGN8Z2xwQyRXBkKY',
-    access_token: '1245096078166417420-2GDKAEfuFV1WH28p7fUjSVtUIVdTcR',
-    access_token_secret: 'q1LD62qCPmmgyznpoiUyj3j0vqD2IlJlgWFXVgY39EtZ4',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL: true,     // optional - requires SSL certificates to be valid.
   }
